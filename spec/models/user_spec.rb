c1651b5c7f6bac8ed6390221184d37d90cb47b3d
kazue-totoro <kazue.kii.1214@gmail.com>
2024-08-15 23:03:15 +0900

    モデルの単体テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it 'nicknameとemail、passwordとpassword_confirmation、family_name、first_name、family_name_kana、first_name_kana、birth_dayが存在すれば登録できる' do
+      it 'nicknameとemail、passwordとpassword_confirmation、family_name、first_name、
+         family_name_kana、first_name_kana、birth_dayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
