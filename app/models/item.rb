5020be104bbc75212d338766e87e3004294e2758
kazue-totoro <kazue.kii.1214@gmail.com>
2024-08-19 20:23:56 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true },
-                    format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                                                    only_integer: true }, format: { with: /\A[0-9]+\z/ }
   validates :user, presence: true
 
   belongs_to :user
