45685030ec45ac1d9e14fc33795d61a9bead01b9
kazue-totoro <kazue.kii.1214@gmail.com>
2024-08-21 20:02:13 +0900

    ビュー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -18,14 +19,17 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
+  def set_item
     @item = Item.find(params[:id])
   end
 
-  def edit
-  end
-
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to  item_path(params[:id])
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
