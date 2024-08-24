3014631543900a3081d65c81ea5922633fc64b73
kazue-totoro <kazue.kii.1214@gmail.com>
2024-08-24 17:25:07 +0900

    単体テストコード作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,8 +5,9 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id, :item_id, :token, :prefecture_id, :city, :block
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'must be a valid phone number' }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'input correctly' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'input only number' }
   end
 
   def save
---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    post_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
+    prefecture_id { 2 }
+    city { Gimei.city.kanji }
+    block { Gimei.town.kanji }
+    phone_number { Faker::Number.number(digits: 11) }
+    token { 'tok_abcdefghijk00000000000000000' }
+  end
+end
