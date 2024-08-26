4adc816839b0aeb9db8cc16f465e8fd64b9eecd8
kazue-totoro <kazue.kii.1214@gmail.com>
2024-08-26 20:21:47 +0900

    クレジットカード決済の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
 
   def index
-    @order_address = OrderAddress.new
+    if (current_user.id == @item.user_id) || @item.order.present?
+      redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      @order_address = OrderAddress.new
+    end
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,7 +31,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:token, :post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(
-      user_id: current_user.id, item_id: params[:item_id]
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = 'sk_test_89bd2e47179f484493fcc000' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
     )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,17 +1,17 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :token, :post_code, :prefecture_id, :city, :block, :building,
-                :phone_number
+                :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :token, :prefecture_id, :city, :block
+    validates :user_id, :item_id, :token, :prefecture_id, :city, :block, :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'input correctly' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'input only number' }
   end
 
   def save
-    order = Order.create(token: token, user_id: user_id, item_id: item_id) # rubocop:disable Style/HashSyntax
+    order = Order.create(user_id: user_id, item_id: item_id) # rubocop:disable Style/HashSyntax
     Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id) # rubocop:disable Style/HashSyntax
   end
 end
